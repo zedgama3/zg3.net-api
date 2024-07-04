689f184e6ecb35f57ed93cf2671f61f887448372
Jared Fowkes <59028480+zedgama3@users.noreply.github.com>
2024-07-04 17:37:28 -0500

    Auto complete client working

--- client/zg3.net-api/src/Autocomplete.js
+++ client/zg3.net-api/src/Autocomplete.js
@@ -1,13 +1,16 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import axios from 'axios';
+import './Autocomplete.css'; // Create and import a CSS file for styling
 
 const Autocomplete = () => {
   const [query, setQuery] = useState('');
   const [suggestions, setSuggestions] = useState([]);
   const [selectedRestaurant, setSelectedRestaurant] = useState(null);
+  const [isSelecting, setIsSelecting] = useState(false);
+  const dropdownRef = useRef(null);
 
   useEffect(() => {
-    if (query.length > 1) {
+    if (!isSelecting && query.length > 1) {
       axios.get(`http://localhost:8012/restaurants/autocomplete/${query}`)
         .then(response => {
           setSuggestions(response.data);
@@ -18,32 +21,42 @@ const Autocomplete = () => {
     } else {
       setSuggestions([]);
     }
-  }, [query]);
+  }, [query, isSelecting]);
 
   const handleSelect = (restaurant) => {
     setSelectedRestaurant(restaurant);
     setQuery(restaurant[0]);
     setSuggestions([]);
+    setIsSelecting(true);
+  };
+
+  const handleInputChange = (e) => {
+    setQuery(e.target.value);
+    setIsSelecting(false);
   };
 
   return (
-    <div>
+    <div className="autocomplete-container">
       <input
         type="text"
         value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        onChange={handleInputChange}
         placeholder="Type to search..."
+        className="autocomplete-input"
+        ref={dropdownRef}
       />
-      <ul>
-        {suggestions.map((restaurant) => (
-          <li key={restaurant[1]} onClick={() => handleSelect(restaurant)}>
-            {restaurant[0]}
-          </li>
-        ))}
-      </ul>
+      {suggestions.length > 0 && (
+        <ul className="autocomplete-suggestions" style={{ top: dropdownRef.current?.offsetHeight }}>
+          {suggestions.map((restaurant) => (
+            <li key={restaurant[1]} onClick={() => handleSelect(restaurant)} className="autocomplete-suggestion">
+              {restaurant[0]}
+            </li>
+          ))}
+        </ul>
+      )}
       {selectedRestaurant && (
-        <div>
-          <p>Selected Restaurant:</p>
+        <div className="restaurant-details">
+          <h2>Restaurant Details</h2>
           <p>Name: {selectedRestaurant[0]}</p>
           <p>ID: {selectedRestaurant[1]}</p>
         </div>
